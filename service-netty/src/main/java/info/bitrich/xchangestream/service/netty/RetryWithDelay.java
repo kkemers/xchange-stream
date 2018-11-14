f85fd6f14e597f1c875f862a08915defa08b31b6
Sergey Lukashevich <sergey.lukashevich@finplant.com>
2018-11-14 14:22:37 +0200

    Tmp

--- service-netty/src/main/java/info/bitrich/xchangestream/service/netty/RetryWithDelay.java
+++ service-netty/src/main/java/info/bitrich/xchangestream/service/netty/RetryWithDelay.java
@@ -1,27 +1,26 @@
 package info.bitrich.xchangestream.service.netty;
 
+import io.reactivex.Completable;
 import io.reactivex.Flowable;
 import io.reactivex.functions.Function;
 import org.reactivestreams.Publisher;
 
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class RetryWithDelay implements Function<Flowable<? extends Throwable>, Publisher<?>> {
     private final long retryDelayMillis;
-    private final AtomicBoolean ignoreRetry;
 
-    public RetryWithDelay(final long retryDelayMillis, AtomicBoolean ignoreRetry) {
+    public RetryWithDelay(final long retryDelayMillis) {
         this.retryDelayMillis = retryDelayMillis;
-        this.ignoreRetry = ignoreRetry;
     }
 
     @Override
-    public Publisher<?> apply(Flowable<? extends Throwable> flowable) {
-        return flowable
-                .takeWhile(e -> !ignoreRetry.get())
-                .flatMap((Function<Throwable, Publisher<?>>) throwable -> {
-                    return Flowable.timer(retryDelayMillis, TimeUnit.MILLISECONDS);
-                });
+    public Publisher<?> apply(Flowable<? extends Throwable> flowable) throws Exception {
+        return flowable.flatMap(new Function<Throwable, Publisher<?>>() {
+            @Override
+            public Publisher<?> apply(Throwable throwable) throws Exception {
+                return Flowable.timer(retryDelayMillis, TimeUnit.MILLISECONDS);
+            }
+        });
     }
 }
