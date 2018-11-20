625e77122125c059308f0a553e3f4e4237b3b3d2
Sergey Lukashevich <sergey.lukashevich@finplant.com>
2018-11-20 09:01:44 +0200

    [Bitfinex] Add logging for incoming orders updates

--- xchange-bitfinex/src/main/java/info/bitrich/xchangestream/bitfinex/BitfinexStreamingPrivateDataService.java
+++ xchange-bitfinex/src/main/java/info/bitrich/xchangestream/bitfinex/BitfinexStreamingPrivateDataService.java
@@ -5,8 +5,12 @@ import info.bitrich.xchangestream.bitfinex.dto.BitfinexWebSocketOrder;
 import info.bitrich.xchangestream.core.StreamingPrivateDataService;
 import io.reactivex.Observable;
 import org.knowm.xchange.dto.Order;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BitfinexStreamingPrivateDataService implements StreamingPrivateDataService {
+
+    private static final Logger log = LoggerFactory.getLogger(BitfinexStreamingPrivateDataService.class);
 
     private final ObjectMapper mapper = new ObjectMapper();
 
@@ -21,6 +25,7 @@ public class BitfinexStreamingPrivateDataService implements StreamingPrivateData
 
         Observable<BitfinexWebSocketOrder> orderSnapshot = streamingService.subscribeChannel("os")
                 .map(json -> mapper.convertValue(json.get(2), BitfinexWebSocketOrder[].class))
+                .doOnNext(order -> log.debug("Order update: {}", (Object[]) order))
                 .flatMap(Observable::fromArray);
         Observable<BitfinexWebSocketOrder> on = streamingService.subscribeChannel("on")
                 .map(json -> mapper.convertValue(json.get(2), BitfinexWebSocketOrder.class));
