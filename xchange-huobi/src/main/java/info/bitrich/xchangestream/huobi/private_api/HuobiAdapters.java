afe0ba13b1c11ded5fc98f99d8361bf6291a9baa
Sergey Lukashevich <sergey.lukashevich@finplant.com>
2019-01-25 18:03:22 +0200

    Add private order object adaptation to common type.

--- xchange-huobi/src/main/java/info/bitrich/xchangestream/huobi/private_api/HuobiAdapters.java
+++ xchange-huobi/src/main/java/info/bitrich/xchangestream/huobi/private_api/HuobiAdapters.java
@@ -4,10 +4,113 @@ import info.bitrich.xchangestream.huobi.private_api.dto.HuobiNotifyMessage;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.Order;
 import org.knowm.xchange.dto.trade.LimitOrder;
+import org.knowm.xchange.dto.trade.MarketOrder;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 public class HuobiAdapters {
-    public static Order adaptOrder(HuobiNotifyMessage huobiNotifyMessage) {
 
-        return new LimitOrder.Builder(Order.OrderType.BID, CurrencyPair.BTC_USD).build();
+    private enum Type {
+        MARKET,
+        LIMIT
+    }
+
+    public static Order adaptOrder(HuobiNotifyMessage message) {
+
+        HuobiNotifyMessage.Data data = message.getData();
+
+        Type type = adaptType(data.getOrderType());
+
+        // TODO: Describe amounts
+
+        if (type == Type.LIMIT) {
+            LimitOrder.Builder builder = (LimitOrder.Builder)
+                    new LimitOrder.Builder(adaptOrderType(data.getOrderType()), adaptCurrencyPair(data.getSymbol()))
+                            .id(data.getOrderId().toString())
+                            .timestamp(new Date(data.getCreatedAt()))
+                            .originalAmount(data.getOrderAmount())
+                            .remainingAmount(data.getUnfilledAmount())
+                            .limitPrice(data.getOrderPrice())
+                            .orderStatus(adaptOrderStatus(data.getOrderState()));
+            LimitOrder limitOrder = builder.build();
+            limitOrder.setFee(data.getFilledFees());
+            return limitOrder;
+        }
+
+        if (type == Type.MARKET) {
+            MarketOrder.Builder builder = (MarketOrder.Builder)
+                    new MarketOrder.Builder(adaptOrderType(data.getOrderType()), adaptCurrencyPair(data.getSymbol()))
+                            .id(data.getOrderId().toString())
+                            .timestamp(new Date(data.getCreatedAt()))
+                            .originalAmount(data.getFilledAmount())
+                            .cumulativeAmount(data.getFilledAmount())
+                            .remainingAmount(BigDecimal.ZERO)
+                            .orderStatus(adaptOrderStatus(data.getOrderState()))
+                            .averagePrice(data.getPrice());
+            MarketOrder marketOrder = builder.build();
+            marketOrder.setFee(data.getFilledFees());
+            return marketOrder;
+        }
+
+        throw new IllegalArgumentException("Unexpected data");
+    }
+
+    private static Order.OrderStatus adaptOrderStatus(String state) {
+        switch (state) {
+            case "submitted":
+                return Order.OrderStatus.NEW;
+            case "partial-filled":
+                return Order.OrderStatus.PARTIALLY_FILLED;
+            case "partial-canceled":
+                return Order.OrderStatus.PARTIALLY_CANCELED;
+            case "filled":
+                return Order.OrderStatus.FILLED;
+            case "canceled":
+                return Order.OrderStatus.CANCELED;
+            default:
+                throw new IllegalArgumentException(String.format("Unknown order state: %s", state));
+        }
+    }
+
+    private static CurrencyPair adaptCurrencyPair(String symbol) {
+        String base = symbol.substring(0, 3).toUpperCase();
+        String counter = symbol.substring(3).toUpperCase();
+        return new CurrencyPair(base, counter);
+    }
+
+    private static Order.OrderType adaptOrderType(String orderType) {
+        switch (orderType) {
+            case "buy-market":
+            case "buy-limit":
+            case "buy-ioc":
+            case "buy-limit-maker":
+                return Order.OrderType.BID;
+            case "sell-market":
+            case "sell-limit":
+            case "sell-ioc":
+            case "sell-limit-maker":
+                return Order.OrderType.ASK;
+            default:
+                throw new IllegalArgumentException(String.format("Unknown order type: %s", orderType));
+
+        }
+    }
+
+    private static Type adaptType(String orderType) {
+        switch (orderType) {
+            case "buy-market":
+            case "sell-market":
+                return Type.MARKET;
+            case "buy-limit":
+            case "buy-limit-maker":
+            case "sell-limit":
+            case "sell-limit-maker":
+            case "buy-ioc":
+            case "sell-ioc":
+                return Type.LIMIT;
+            default:
+                throw new IllegalArgumentException(String.format("Unknown order type: %s", orderType));
+        }
     }
 }
--- xchange-huobi/src/test/java/info/bitrich/xchangestream/huobi/HuobiJsonTests.java
+++ xchange-huobi/src/test/java/info/bitrich/xchangestream/huobi/HuobiJsonTests.java
@@ -6,10 +6,16 @@ import info.bitrich.xchangestream.huobi.private_api.HuobiPrivateStreamingService
 import info.bitrich.xchangestream.huobi.public_api.HuobiPublicStreamingService;
 import io.reactivex.Observable;
 import io.reactivex.observers.TestObserver;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.dto.Order;
 import org.knowm.xchange.dto.marketdata.OrderBook;
+import org.knowm.xchange.dto.trade.MarketOrder;
+
+import java.math.BigDecimal;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -28,7 +34,7 @@ public class HuobiJsonTests {
     }
 
     @Test
-    public void testGetOrderExecution_orderPlace() throws Exception {
+    public void testMarketDepth() throws Exception {
 
         JsonNode jsonNode = objectMapper.readTree(ClassLoader.getSystemClassLoader()
                 .getResourceAsStream("market-depth.json"));
@@ -41,5 +47,26 @@ public class HuobiJsonTests {
         observer.assertNoErrors();
         observer.assertValueCount(1);
     }
-}
 
+    @Test
+    public void testGetOrder_MarketBuy() throws Exception {
+
+        JsonNode jsonNode = objectMapper.readTree(ClassLoader.getSystemClassLoader()
+                .getResourceAsStream("order-market-buy.json"));
+
+        when(privateStreamingService.subscribeChannel("orders.*")).thenReturn(Observable.just(jsonNode));
+
+        TestObserver<Order> observer = exchange.getStreamingPrivateDataService().getOrders().test();
+
+        observer.assertNoErrors();
+        observer.assertValueCount(1);
+
+        MarketOrder order = (MarketOrder) observer.values().get(0);
+        Assert.assertEquals(order.getId(), "22969278037");
+        Assert.assertEquals(order.getCurrencyPair(), new CurrencyPair(Currency.BTC, Currency.USDT));
+        Assert.assertEquals(order.getOriginalAmount(), new BigDecimal("0.000281383831846703"));
+        Assert.assertTrue(order.getRemainingAmount().compareTo(BigDecimal.ZERO) == 0);
+        Assert.assertEquals(order.getAveragePrice(), new BigDecimal("3553.864461355386443569"));
+        Assert.assertEquals(order.getFee(), new BigDecimal("0.000000562767663693"));
+    }
+}
